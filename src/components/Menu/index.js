e0c1e7e8ba7e6a9a280cdd5c804c31dd789e7dfd
Ariane Silva <dev.arianess@gmail.com>
2020-07-28 21:22:22 -0300

    Inclusão das páginas de cadastro vídeo e categoria

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,17 +1,19 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Logo from '../../assets/img/Logo.png';
+import './Menu.css';
 import Button from '../Button';
 // import ButtonLink from './components/ButtonLink';
-import './Menu.css';
+
 
 function Menu() {
     return (
         <nav className="Menu">
-            <a href="/">
+            <Link to="/">
                 <img className="Logo" src={Logo} alt="ArianeFlix logo" />
-            </a>
+            </Link>
 
-            <Button as="a" className="ButtonLink" href="/">
+            <Button as={Link} className="ButtonLink" to="/cadastro/video">
                 Novo vídeo
             </Button>
         </nav>
--- src/index.js
+++ src/index.js
@@ -1,11 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import CadastroVideo from './pages/cadastro/Video';
+import CadastroCategoria from './pages/cadastro/Categoria';
+
+const Page404 = () => (
+  <div>Página não encontrada!</div>
+);
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <Switch>
+      <Route path="/" component={Home} exact />
+      <Route path="/cadastro/video" component={CadastroVideo} />
+      <Route path="/cadastro/categoria" component={CadastroCategoria} />
+      <Route component={Page404} />
+    </Switch>
+  </BrowserRouter>,
   document.getElementById('root')
 );
